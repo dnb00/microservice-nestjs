1481b92884ff88bc397f185fb939482ec22c6044
Daniel <danielbatista.thoughtworks@latam.com>
2024-02-24 13:30:08 -0300

    payment microservice

---
+++ apps/payments/src/payments.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
+import { CreateChargeDto } from '../../../libs/common/src/dto/create-charge.dto';
+
+@Injectable()
+export class PaymentsService {
+  constructor(private readonly configService: ConfigService) {}
+
+  private readonly stripe = new Stripe(
+    this.configService.get('STRIPE_SECRET_KEY'),
+    {
+      apiVersion: '2023-10-16',
+    },
+  );
+
+  async createCharge({ card, amount }: CreateChargeDto) {
+    // const paymentMethod = await this.stripe.paymentMethods.create({
+    //   type: 'card',
+    //   card,
+    // });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: 'pm_card_visa',
+      amount: amount * 100,
+      confirm: true,
+      currency: 'usd',
+      automatic_payment_methods: { enabled: true, allow_redirects: 'never' },
+    });
+
+    return paymentIntent;
+  }
+}
